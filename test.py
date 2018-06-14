6f6c1e1e0cceedb51ab36b404c27ce5ae154b6b9
Doan Dong Nguyen <doandongnguyen@gmail.com>
2018-06-14 22:23:23 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+
 
 def draw_label(rects, names):
     """TODO: Docstring for draw_label.
@@ -16,20 +18,34 @@ def draw_label(rects, names):
 
 df = pd.read_excel('./Datedisertatie.xlsx')
 
-bar_list=[]
-x_names = []
-columns_name = df.columns.values.tolist()
-for i in range(1, len(df.columns)-2, 4):
-    dif = df.iloc[-1,i]-df.iloc[-1,i+2]
-    if (abs(dif) > 10):
-        bar_list.append(dif)
-        x_names.append(str(columns_name[i]))
+possitive_bar_list = []
+negative_bar_list = []
+possitive_x_names = []
+negative_x_names = []
 
-print(len(columns_name))
-print(df.describe())
-x = range(len(bar_list))
-# plt.figure(figsize=(12, 8))
+columns_name = df.columns.values.tolist()
+
+print('There are:', len(columns_name), ' columns')
+for i in range(1, len(df.columns)-2, 3):
+    dif = df.iloc[-1,i]-df.iloc[-1,i+2]
+    if (dif > 0):
+        possitive_bar_list.append(dif)
+        possitive_x_names.append(str(columns_name[i]))
+    else:
+        negative_bar_list.append(dif)
+        negative_x_names.append(str(columns_name[i]))
+
+width = 0.5
+x1 = 2*np.arange(len(possitive_bar_list))
+x2 = 2*np.arange(len(negative_bar_list)) + 1
+print(x1)
+print(x2)
 fig, ax = plt.subplots()
-rects = ax.bar(x, bar_list, width=0.2, alpha=0.8)
-draw_label(rects, x_names)
+positive_rects = ax.bar(x1, possitive_bar_list, width=width, alpha=0.8,
+                        color='b')
+negative_rects = ax.bar(x2, negative_bar_list, width=width, alpha=0.8,
+                        color='r')
+
+draw_label(positive_rects, possitive_x_names)
+draw_label(negative_rects, negative_x_names)
 plt.show()
