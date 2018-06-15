fcaf0b12ae94df2689dd0d190510f1dec1e32403
Doan Dong Nguyen <doandongnguyen@gmail.com>
2018-06-15 16:15:17 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def draw_label(rects, names):
+def draw_label(ax, rects, names):
     """TODO: Docstring for draw_label.
 
     :rect: TODO
@@ -13,11 +13,11 @@ def draw_label(rects, names):
     for i, rect in enumerate(rects):
         height = rect.get_height()
         ax.text(rect.get_x() + rect.get_width()/2.0,
-                1.05*height, '%s' %names[i],
+                1.05*height, '%s' % names[i][:-6],
                 ha='center', va='bottom')
 
-df = pd.read_excel('./Datedisertatie.xlsx')
 
+df = pd.read_excel('./Datedisertatie.xlsx')
 possitive_bar_list = []
 negative_bar_list = []
 possitive_x_names = []
@@ -26,8 +26,9 @@ negative_x_names = []
 columns_name = df.columns.values.tolist()
 
 print('There are:', len(columns_name), ' columns')
-for i in range(1, len(df.columns)-2, 3):
-    dif = df.iloc[-1,i]-df.iloc[-1,i+2]
+for i in range(1, len(df.columns)-2, 4):
+    print(columns_name[i], ':', columns_name[i+2])
+    dif = df.iloc[-1, i] - df.iloc[-1, i+2]
     if (dif > 0):
         possitive_bar_list.append(dif)
         possitive_x_names.append(str(columns_name[i]))
@@ -35,17 +36,15 @@ for i in range(1, len(df.columns)-2, 3):
         negative_bar_list.append(dif)
         negative_x_names.append(str(columns_name[i]))
 
-width = 0.5
+width = 0.3
 x1 = 2*np.arange(len(possitive_bar_list))
 x2 = 2*np.arange(len(negative_bar_list)) + 1
-print(x1)
-print(x2)
 fig, ax = plt.subplots()
 positive_rects = ax.bar(x1, possitive_bar_list, width=width, alpha=0.8,
-                        color='b')
+                        color='b', label='Positive mean')
 negative_rects = ax.bar(x2, negative_bar_list, width=width, alpha=0.8,
-                        color='r')
+                        color='r', label='Negative mean')
 
-draw_label(positive_rects, possitive_x_names)
-draw_label(negative_rects, negative_x_names)
+draw_label(ax, positive_rects, possitive_x_names)
+draw_label(ax, negative_rects, negative_x_names)
 plt.show()
